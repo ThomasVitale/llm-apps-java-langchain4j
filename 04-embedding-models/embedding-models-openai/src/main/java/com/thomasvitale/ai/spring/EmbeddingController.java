a5a9b700f61095f6e7df631d49fabcc54570494a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-01-15 22:15:42 +0100

    Add embedding models samples

---
+++ 04-embedding-models/embedding-models-openai/src/main/java/com/thomasvitale/ai/spring/EmbeddingController.java
@@ -0,0 +1,23 @@
+package com.thomasvitale.ai.spring;
+
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EmbeddingController {
+
+    private final EmbeddingModel embeddingModel;
+
+    EmbeddingController(EmbeddingModel embeddingModel) {
+        this.embeddingModel = embeddingModel;
+    }
+
+    @GetMapping("/ai/embed")
+    String embed(@RequestParam(defaultValue = "And Gandalf yelled: 'You shall not pass!'") String message) {
+        var embeddings = embeddingModel.embed(message);
+        return "Size of the embedding vector: " + embeddings.content().vectorAsList().size();
+    }
+
+}
