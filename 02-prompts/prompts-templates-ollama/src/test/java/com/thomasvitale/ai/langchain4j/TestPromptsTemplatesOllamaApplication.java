e406177531839232946d10a5c2d2877788390a56
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-03-10 23:20:25 +0100

    Update tests

--- 02-prompts/prompts-templates-ollama/src/test/java/com/thomasvitale/ai/langchain4j/TestPromptsTemplatesOllamaApplication.java
+++ 02-prompts/prompts-templates-ollama/src/test/java/com/thomasvitale/ai/langchain4j/TestPromptsTemplatesOllamaApplication.java
@@ -1,11 +1,12 @@
 package com.thomasvitale.ai.langchain4j;
 
-import io.thomasvitale.langchain4j.testcontainers.service.containers.OllamaContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestPromptsTemplatesOllamaApplication {
@@ -14,7 +15,8 @@ public class TestPromptsTemplatesOllamaApplication {
     @RestartScope
     @ServiceConnection
     OllamaContainer ollama() {
-        return new OllamaContainer("ghcr.io/thomasvitale/ollama-llama2");
+        return new OllamaContainer(DockerImageName.parse("ghcr.io/thomasvitale/ollama-llama2")
+                .asCompatibleSubstituteFor("ollama/ollama"));
     }
 
     public static void main(String[] args) {
